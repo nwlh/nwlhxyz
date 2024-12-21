c13ce94566acd9d7f71107f632b1971c2c6614ed
newlyghter <stigma-peeling-0r@icloud.com>
2024-12-21 22:32:17 +0100

    added button

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,17 @@
+<script>
+    let count = $state(0);
+
+    function increment() {
+        count += 100;
+    }
+</script>
+
 <h1>nwlh.xyz</h1>
+
+<button onclick={increment}>
+    donated {count}
+    {count === 1 ? "dollar" : "dollars"}
+</button>
 
 <style>
     h1 {
